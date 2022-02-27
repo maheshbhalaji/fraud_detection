3a0e5b8e7efc7e71ddd8757568b543f7aa9e7641
maheshbhalaji <maheshbhalaji@gmail.com>
2022-02-27 23:24:22 +0530

    model2 is the final model

--- ml_model.py
+++ ml_model.py
@@ -8,8 +8,10 @@ def model1():
 
 
 def model2():
-    print("adding code for ML Model1")
+    print("adding code for ML Model2")
 
 
 def model3():
-    print("adding code for ML Model1")
+    print("adding code for ML Model3")
+
+print("model2 is performing the best")
